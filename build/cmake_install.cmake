500d32e57032d486cb6cf5c41d1efbda0426ba87
Your Name Here <Same e-mail as used for github>
2025-07-28 00:45:39 +0800

    小车模型

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -132,6 +132,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/slz/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/slz/catkin_ws/build/fsac_track_worlds/cmake_install.cmake")
+  include("/home/slz/catkin_ws/build/racecar_description/cmake_install.cmake")
 
 endif()
 
--- build/racecar_description/CTestTestfile.cmake
+++ build/racecar_description/CTestTestfile.cmake
@@ -1,9 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/slz/catkin_ws/src
-# Build directory: /home/slz/catkin_ws/build
+# Source directory: /home/slz/catkin_ws/src/racecar_description
+# Build directory: /home/slz/catkin_ws/build/racecar_description
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("fsac_track_worlds")
-subdirs("racecar_description")
