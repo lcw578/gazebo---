cdc33df9045c8ff3d93832b16121f4dada432481
Your Name Here <Same e-mail as used for github>
2025-07-27 23:55:46 +0800

    构建gazebo地图

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/slz/catkin_ws/src
+# Build directory: /home/slz/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("fsac_track_worlds")
---
+++ build/fsac_track_worlds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/slz/catkin_ws/src/fsac_track_worlds
+# Build directory: /home/slz/catkin_ws/build/fsac_track_worlds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
